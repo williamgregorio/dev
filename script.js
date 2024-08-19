255b098906a11c91f2c7de5cf7b2ea7051c02e2e
William Gregorio <mail@williamgregorio.com>
2024-08-19 01:22:43 -0400

    del on dom real popup

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const skills = [
-  {name:"HTML5", path:"html5"},
-  {name:"CSS3", path:"css3"},
-  {name:"JavaScript", path:"javascript"},
-  {name:"PHP", path:"php"},
-  {name:"Mautic", path:"mautic"},
-  {name:"Matomo", path:"matomo"},
-  {name:"Google AdWords", path:"googleads"},
-  {name:"GA4", path:"google-analytics"},
-  {name:"Figma", path:"figma"},
-  {name:"Salesforce", path:"salesforce"},
-  {name:"WordPress", path:"wordpress"},
-  {name:"Shopify", path:"shopify"}
-]
+  { name: "HTML5", path: "html5" },
+  { name: "CSS3", path: "css3" },
+  { name: "JavaScript", path: "javascript" },
+  { name: "PHP", path: "php" },
+  { name: "Mautic", path: "mautic" },
+  { name: "Matomo", path: "matomo" },
+  { name: "Google AdWords", path: "googleads" },
+  { name: "GA4", path: "google-analytics" },
+  { name: "Figma", path: "figma" },
+  { name: "Salesforce", path: "salesforce" },
+  { name: "WordPress", path: "wordpress" },
+  { name: "Shopify", path: "shopify" },
+];
 
 const emailTypes = {
   transactional: [
@@ -19,36 +19,29 @@ const emailTypes = {
     "Shipping Confirmation",
     "Password Reset",
     "Account Creation",
-    "Payment Receipt"
+    "Payment Receipt",
   ],
   promotional: [
     "Product Announcements",
     "Sales and Discounts",
     "Newsletters",
     "Event Invitations",
-    "Seasonal Campaigns"
+    "Seasonal Campaigns",
   ],
-  engagement: [
-    "Welcome Emails",
-    "Re-engagement Emails",
-    "Survey Requests"
-  ],
+  engagement: ["Welcome Emails", "Re-engagement Emails", "Survey Requests"],
   behavioral: [
     "Abandoned Cart",
     "Browse Abandonment",
-    "Product Recommendations"
+    "Product Recommendations",
   ],
-  informational: [
-    "Company Updates",
-    "Educational Content"
-  ]
+  informational: ["Company Updates", "Educational Content"],
 };
 
-// Skill card dom rendering 
+// Skill card dom rendering
 function SkillCard(name, path) {
-  const cardDiv  = document.createElement("div");
+  const cardDiv = document.createElement("div");
   cardDiv.setAttribute("class", "skill");
-  
+
   const logoNameSpan = document.createElement("span");
   logoNameSpan.textContent = name;
 
@@ -56,17 +49,17 @@ function SkillCard(name, path) {
   imageSVGDiv.style.backgroundImage = `url("assets/${path}.svg")`;
   imageSVGDiv.className += "skill-image";
 
-  cardDiv.append(logoNameSpan,imageSVGDiv);
+  cardDiv.append(logoNameSpan, imageSVGDiv);
   return cardDiv;
 }
 
 // Displays skills logo
 const skillList = document.querySelector("#skill-list");
 
-skills.forEach(skill => {
-  skillList.append(SkillCard(skill.name, skill.path))
+skills.forEach((skill) => {
+  skillList.append(SkillCard(skill.name, skill.path));
   return skillList;
-})
+});
 
 // Scroll to top
 const heroAnchor = document.querySelector("#scroll-to-top");
@@ -74,7 +67,7 @@ heroAnchor.addEventListener("click", (e) => {
   e.preventDefault();
   window.scrollTo({
     top: 0,
-    behavior: "instant"
+    behavior: "instant",
   });
 });
 
@@ -89,8 +82,8 @@ const emailsAnchor = document.querySelector(".scroll-to-emails");
 function scrollUser(anchor, section) {
   anchor.addEventListener("click", (e) => {
     e.preventDefault();
-    section.scrollIntoView({behavior: "instant", block: "start"});
-  })
+    section.scrollIntoView({ behavior: "instant", block: "start" });
+  });
 }
 
 scrollUser(skillsAnchor, skillsSection);
@@ -111,11 +104,22 @@ document.addEventListener("scroll", () => {
 //Displays list of emails
 const emailListDiv = document.querySelector("#email-list");
 const emails = [
-  {type: emailTypes.promotional[0], description: "A responsive email for a new product announcement.", imageFileName:"katz-newsletter-portfolio.png", filename: "katz-promotional-product-announcement.html"},
-  {type: emailTypes.transactional[4], description: "A responsive new payment receipt email for a service business.", imageFileName:"stripe-first-transaction.png", filename: "stripe-transactional-payment-receipt.html"},
-]
+  {
+    type: emailTypes.promotional[0],
+    description: "A responsive email for a new product announcement.",
+    imageFileName: "katz-newsletter-portfolio.png",
+    filename: "katz-promotional-product-announcement.html",
+  },
+  {
+    type: emailTypes.transactional[4],
+    description:
+      "A responsive new payment receipt email for a service business.",
+    imageFileName: "stripe-first-transaction.png",
+    filename: "stripe-transactional-payment-receipt.html",
+  },
+];
 
-function EmailCard(type,description,imageFileName,filename){
+function EmailCard(type, description, imageFileName, filename) {
   const rootPath = "assets";
   let emailCardDiv = document.createElement("div");
   let emailCardMetaDiv = document.createElement("div");
@@ -127,31 +131,38 @@ function EmailCard(type,description,imageFileName,filename){
   metaTypeHeadingThree.textContent = `Type: ${type}`;
   let metaDescriptionParagraph = document.createElement("p");
   metaDescriptionParagraph.textContent = `Description: ${description}`;
-  emailCardMetaDiv.append(metaTypeHeadingThree,metaDescriptionParagraph);
+  emailCardMetaDiv.append(metaTypeHeadingThree, metaDescriptionParagraph);
 
   emailCardAnchorImage.setAttribute("target", "_blank");
   emailCardAnchorImage.setAttribute("href", `emails/${filename}`);
   emailCardAnchorImage.className += "email-image";
-  emailCardAnchorImage.style.backgroundImage = `url("${rootPath}/${imageFileName}")`
-  emailCardDiv.append(emailCardMetaDiv,emailCardAnchorImage);
+  emailCardAnchorImage.style.backgroundImage = `url("${rootPath}/${imageFileName}")`;
+  emailCardDiv.append(emailCardMetaDiv, emailCardAnchorImage);
   return emailCardDiv;
 }
 
-emails.forEach(email => {
-  emailListDiv.append(EmailCard(email.type, email.description, email.imageFileName, email.filename));
-})
+emails.forEach((email) => {
+  emailListDiv.append(
+    EmailCard(
+      email.type,
+      email.description,
+      email.imageFileName,
+      email.filename,
+    ),
+  );
+});
 
 //active state evaluator
 const observerOptions = {
   root: null,
-  rootMargin: '0px', 
-  threshold: 0.7
-}
+  rootMargin: "0px",
+  threshold: 0.7,
+};
 
 const sections = document.querySelectorAll("sections");
 
 const observerCallback = (entries) => {
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     const id = entry.target.getAttribute("id");
     const navLink = document.querySelector(`nav a[href="#${id}"]`);
 
@@ -161,13 +172,13 @@ const observerCallback = (entries) => {
       navLink.classList.remove("active");
     }
   });
-}
+};
 
 const observer = new IntersectionObserver(observerCallback, observerOptions);
 
-document.querySelectorAll("section").forEach(section => {
+document.querySelectorAll("section").forEach((section) => {
   observer.observe(section);
-})
+});
 
 /*
 READ ALOUD TIME
@@ -206,7 +217,7 @@ function stopTimer() {
 
   scrollTimes.push({
     cycle: scrollCount + 1,
-    timeTaken: timeTaken
+    timeTaken: timeTaken,
   });
 
   timerRunning = false;
@@ -230,39 +241,30 @@ function checkUserScrollPosition() {
     if (timerRunning) {
       console.log(`Cycle number: ${scrollCount + 1}, touched bottom document`);
       stopTimer();
+      console.log(scrollTimes);
     }
   } else if (scrollTop === 0) {
     if (!timerRunning && scrollCount < maxScrollCount) {
-      console.log("User touched top of page, resetting timer.")
+      console.log("User touched top of page, resetting timer.");
       startTimer();
     }
   }
 }
 
-function showPopup() {
-  const popup = document.querySelector("#popup");
-  const popupXDiv = document.createElement("div");
-  popupXDiv.className += "close-popup";
-  const popupXIMG= document.createElement("img");
-  popupXIMG.className += "x-close";
-  popupXIMG.setAttribute("src", `assets/close-bold.svg`);
-  popupXDiv.append(popupXIMG);
-  popup.append(popupXDiv);
-  popup.style.display = "block";
-}
+const main = document.querySelector("main");
 
-function closePopup() {
-  document.querySelector("#popup").style.display = "none";
-}
+function Popup() {}
+function showPopup() {}
+function closePopup() {}
 
 function handleNocrollUpTimeout() {
   timerTimeout = setTimeout(() => {
     if (!timerRunning) {
+      window.removeEventListener("scroll", checkUserScrollPosition);
       console.log("No scroll up detected after detected, displaying data");
       showPopup();
-      window.removeEventListener("scroll", checkUserScrollPosition);
     }
-  }, 10000)
+  }, 10000);
 }
 
 function inititalizeScrollTracking() {
@@ -272,4 +274,3 @@ function inititalizeScrollTracking() {
 }
 
 window.addEventListener("load", inititalizeScrollTracking);
-
