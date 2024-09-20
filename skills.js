8e019bd0b8be2a0ccbe7a17b14777481dc29b2e1
William Gregorio <mail@williamgregorio.com>
2024-09-19 21:41:53 -0400

    testing get on json

---
+++ skills.js
@@ -0,0 +1,26 @@
+async function getSkills() {
+  try {
+    const response = await fetch("https://williamgregorio.com/data/skills.json", {
+      method: "GET",
+      headers: {"Content-Type": "application/json"}
+    });
+    if (!response.ok) {
+      throw new Error("Network request failed.", response.status + ' ' + response.statusText);
+    }
+
+    const result = await response.json();
+    console.log(result)
+    return result;
+  } catch (error) {
+    console.error("Error: ", error.message);
+    return null;
+  }
+}
+
+getSkills().then(skills => {
+  if (skills) {
+    console.log(skills);
+  } else {
+    console.log("No skills to process came back :(");
+  }
+});
