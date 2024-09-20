df98dc9b3911f84bfd1ed20459180fb19730ae4f
William Gregorio <mail@williamgregorio.com>
2024-09-20 01:08:51 -0400

    EmailCard dom element added

--- emails.js
+++ emails.js
@@ -34,9 +34,39 @@ async function getEmails() {
 getEmails().then(emails => {
   if (emails && emails.length > 0) {
     console.log(emails);
+    const emailListDiv = document.querySelector("#email-list");
+
   } else {
     console.log("No emails to process came back.");
   }
 }).catch(error => {
     console.error("Error: ", error);
 })
+
+function EmailCard(type, description, imageFile, fileName) {
+  const root = "assets";
+
+  let emailCardDiv = document.createElement("div");
+  let emailCardMetaDiv = document.createElement("div");
+  let emailCardAnchorImage = document.createElement("img");
+
+  emailCardDiv.className += "email-card";
+  emailCardMetaDiv.className += "email-card-meta";
+  
+  let metaTypeHeadingThree = document.createElement("h3");
+  metaTypeHeadingThree.textContent = `${type}`;
+
+  let metaDescriptionParagraph = document.createElement("p");
+  metaDescriptionParagraph.textContent = `${description}`;
+
+  emailCardDiv.append(metaTypeHeadingThree, metaDescriptionParagraph);
+
+  emailCardAnchorImage.setAttribute("target", "_blank");
+  emailCardAnchorImage.setAttribute("href", `emails/${fileName}`);
+  emailCardAnchorImage.className += "email-image";
+  emailCardAnchorImage.style.backgroundImage = `url("${root}/${imageFile}")`;
+
+  emailCardDiv.append(emailCardMetaDiv, emailCardAnchorImage);
+  return emailCardDiv;
+}
+
