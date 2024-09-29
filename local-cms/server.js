021bade3045f7400d9046e89b85cff1b98aa40fa
William Gregorio <mail@williamgregorio.com>
2024-09-28 23:49:59 -0400

    send test and came low

--- local-cms/server.js
+++ local-cms/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
+const port = 3000;
 
 app.use(express.json());
 app.use(express.static("../"));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+app.get("/", (req,res) => {
+  res.send("Local CMS");
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 })
