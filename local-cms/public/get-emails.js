ab7f3a5a0695312f076b0dbc31c280bf157083b9
William Gregorio <mail@williamgregorio.com>
2024-10-01 01:49:17 -0400

    mv to n file

--- local-cms/public/get-emails.js
+++
@@ -1,9 +0,0 @@
-import { fetchData } from './lib/fetch-data.js';
-
-fetchData("http://localhost:3000/api/emails").then(data => {
-  console.log(data);
-}).catch(err => {
-    console.error(err);
-  })
-
-
