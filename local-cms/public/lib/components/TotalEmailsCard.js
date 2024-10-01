a1ed8800e63b144e2bf28d4f54a2f362182c08f1
William Gregorio <mail@williamgregorio.com>
2024-10-01 02:51:41 -0400

    basic comp for testting

--- local-cms/public/lib/components/TotalEmailsCard.js
+++ local-cms/public/lib/components/TotalEmailsCard.js
@@ -1,8 +1,14 @@
-function TotalEmailsCard(){
+function TotalEmailsCard(number, text){
   const div = document.createElement("div");
+  div.id = "total-emails";
   div.style.borderRadius = "12px";
   div.style.padding = "2em";
 
+  const headingText = document.createElement("h2");
+  headingText.textContent += text;
+  let numberCount = document.createElement("p");
+  numberCount.textContent += number;
+  div.append(headingText, numberCount);
   return div;
 }
 
