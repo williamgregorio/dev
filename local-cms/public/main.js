8d2dff85b9490048e2da8091b01332844e360487
William <mrwilliamgregorio@yahoo.com>
2024-10-07 02:31:20 -0400

    edit prep for put

--- local-cms/public/main.js
+++ local-cms/public/main.js
@@ -7,15 +7,16 @@ fetchData(`${host}api/emails`).then(data => {
   let allEmails = document.querySelector('#all-emails');
   data.forEach((email,index) => {
     console.log(index);
-   allEmails.append(EmailCard(email.type, email.description, email.imageFileName));
+   allEmails.append(EmailCard(email.type, email.description, email.imageFileName, index));
   });
 
 }).catch(err => {
     console.error(err);
   })
 
-function EmailCard(type,description,image) {
+function EmailCard(type,description,image,index) {
   let div = document.createElement('div');
+  div.setAttribute('data-index', index);
   div.className = 'email-card';
   let emailTitle = document.createElement('h2');
   emailTitle.textContent += 'Title';
@@ -27,7 +28,14 @@ function EmailCard(type,description,image) {
   emailThumbnail.setAttribute('src', `http://127.0.0.1:8080/assets/${image}`);
   emailThumbnail.setAttribute('width', '100px');
 
-  div.append(emailTitle,emailType,emailDescription, emailThumbnail);
+  let editButton = document.createElement('button');
+  editButton.textContent += 'Edit';
+
+  editButton.addEventListener('click', () => {
+    console.log('click');
+  });
+
+  div.append(emailTitle,emailType,emailDescription, emailThumbnail, editButton);
   return div;
 }
 
