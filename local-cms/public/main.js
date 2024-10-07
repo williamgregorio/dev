2b85778c8ee12531073e95cd8606a30518e2a415
William <mrwilliamgregorio@yahoo.com>
2024-10-07 07:56:36 -0400

    created on emails div and render prep

--- local-cms/public/main.js
+++ local-cms/public/main.js
@@ -1,9 +1,15 @@
 import { rendersEmailPage } from './lib/emails.js';
 
 const host = location.href;
-
+let main = document.querySelector('main');
 document.querySelector('#emails-link').addEventListener('click', async (e) => {
   e.preventDefault();
+  const allEmails = document.createElement('div');
+  allEmails.id = 'all-emails';
+  main.append(allEmails);
   await rendersEmailPage();
 });
 
+function rendersDashboard() {
+
+}
