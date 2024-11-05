45f84cce859e64246bbbe12d2d76e46234f2d71c
William <mrwilliamgregorio@yahoo.com>
2024-11-05 03:10:59 -0500

    added creation function on desired path

--- projectManager.js
+++ projectManager.js
@@ -8,3 +8,30 @@ const rl = readline.createInterface({
 });
 
 const emailProjectPath = path.join(process.cwd(), "emails", "development");
+
+function createProjectName(projectName) {
+  const projectDir = path.join(emailProjectPath, projectName);
+  const mjmlTemplateFile = path.join(emailProjectPath, "index.mjml");
+  const assetsDir = path.join(emailProjectPath, "assets");
+  const emailMJMLTemplate = `
+  <mjml>
+    <mj-head>
+      <mj-title>
+      
+      </mj-title>
+    </mj-head>
+    <mj-body>
+
+    </mj-body>
+  </mjml>`;
+
+  if (!fs.existsSysnc(projectDir)) {
+    fs.mkdirSync(projectDir, { recursive: true });
+    fs.mkdirSync(assetsDir);
+    fs.writeFileSync(mjmlTemplateFile, emailMJMLTemplate);
+    console.log(`New project ${projectName}, has been created.`);
+  } else {
+    console.log(`Project: ${projectName}, already exists.`);
+  }
+}
+
