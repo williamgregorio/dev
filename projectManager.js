56bee5acf3d9caca66fcea1effd4ef76f7e1b37e
William <mrwilliamgregorio@yahoo.com>
2024-11-05 02:56:48 -0500

    added interface creation

--- projectManager.js
+++ projectManager.js
@@ -2,4 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
 
-
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
